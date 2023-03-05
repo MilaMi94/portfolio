586a08a3739d28143495ac1b41bc2f56999cba37
MilaMi94 <mila.bogdanovic94@gmail.com>
2023-03-05 10:50:42 +0100

    input sass

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import "./topbar.scss";
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#works">asdas </a>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
